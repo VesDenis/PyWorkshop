32a93e39be429d74d54a0e37a47c65bea140adf5
Денис <103391833+VesDenis@users.noreply.github.com>
2022-04-11 02:48:51 +0000

    bad santa

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-my_list[-1:] -> [2187]
-my_list[:] -> [3, 9, 27, 81, 243, 729, 2187]
-my_list[2:-1:-3] -> []
-my_list[2:0:-3] -> [27]
-my_list[7]-> IndexError
+N = int(input())
+K = int(input())
+while N < K:
+  N += N
+print(N - K)
