bda5d4652cfc1f4d4e46f38d634d5e7d0e96e89f
Денис <103391833+VesDenis@users.noreply.github.com>
2022-04-11 02:21:33 +0000

    default slicing

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
-Doctor Who, Friends
+[0:len(sequence):1]
+[0:len(sequence):0]
+[1:len(sequence):1]
+[1:len(sequence):0]
+[0:len(sequence) - 1:1]
+[0:len(sequence) - 1:0]
+[1:len(sequence) - 1:1]
+[1:len(sequence) - 1:0]
