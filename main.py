5b7377a41cf9a529cf50478a33d48807f1ebb427
Денис <103391833+VesDenis@users.noreply.github.com>
2022-04-12 01:35:18 +0000

    re-write

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
-letters = list(input().split())
-vowels = ['a', 'o', 'e', 'u', 'y']
-print(list(filter(lambda letter: letter in vowels, letters)))
+even = [0, 2, 4, 6, 8]
+odd = [1, 3, 5, 7, 9]
+
+my_sum = list(map(lambda x, y: x + y, even, odd))
+
+remainders = list(map(lambda x: x % 3, my_sum))
+
+nonzero_remainders = list(filter(lambda x: x, remainders))
