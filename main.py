ccd3d2a148c8496f2210ab6e850f9deda141fccd
Денис <103391833+VesDenis@users.noreply.github.com>
2022-04-12 02:19:10 +0000

    equal

--- main.py
+++ main.py
@@ -1,8 +1,1 @@
-from random import randint
-
-students = ['Денис', 'Дима', 'Ваня', 'Дима', 'Дима', 'Лёха', 'Вова', 'Вадим', 'Богдан', 'Юра', 'Антон', 'Артем', 'Костя']
-math = [randint(25, 50) for i in range(13)]
-physics = [randint(25, 50) for i in range(13)]
-english = [randint(25, 50) for i in range(13)]
-grades = list(map(lambda x, y, z: x + y + z, math, physics, english))
-print([students[i] for i in range(13) if grades[i] >= 100])
+a, c
