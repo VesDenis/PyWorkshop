d130f2ca4783a4f54bdcdcfe68332ca8d80bf4a3
Денис <103391833+VesDenis@users.noreply.github.com>
2022-04-12 01:27:43 +0000

    vowels

--- main.py
+++ main.py
@@ -1,27 +1,3 @@
-from random import randint
-
-def shift(a):
-  k = randint(1, len(a) - 1) * -1
-  a = a[k:] + a[:k]
-  return a
-
-def search(a):
-  arr = a[:]
-  l = a[len(a) - 1]
-  if a[0] < a[len(a) - 1]:
-    return a[len(a) - 1]
-  else:
-    while arr[len(arr) // 2] < a[len(arr) // 2 + 1]:
-      if arr[len(arr) // 2] < l:
-        arr = arr[:len(arr) // 2]
-      else:
-        arr = arr[len(arr) // 2:]
-      print(arr)
-    return arr[len(arr) // 2]
-      
-
-a = [_ for _ in range(1, 11)]
-a.sort()
-a = shift(a)
-print(a)
-print(search(a))
+letters = list(input().split())
+vowels = ['a', 'o', 'e', 'u', 'y']
+print(list(filter(lambda letter: letter in vowels, letters)))
