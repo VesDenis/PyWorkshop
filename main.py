df09a7d326601aa0001d84f05e40e2a56c44bb8d
Денис <103391833+VesDenis@users.noreply.github.com>
2022-04-12 01:11:18 +0000

    binary_search

--- main.py
+++ main.py
@@ -1,5 +1,27 @@
-N = int(input())
-K = int(input())
-while N < K:
-  N += N
-print(N - K)
+from random import randint
+
+def shift(a):
+  k = randint(1, len(a) - 1) * -1
+  a = a[k:] + a[:k]
+  return a
+
+def search(a):
+  arr = a[:]
+  l = a[len(a) - 1]
+  if a[0] < a[len(a) - 1]:
+    return a[len(a) - 1]
+  else:
+    while arr[len(arr) // 2] < a[len(arr) // 2 + 1]:
+      if arr[len(arr) // 2] < l:
+        arr = arr[:len(arr) // 2]
+      else:
+        arr = arr[len(arr) // 2:]
+      print(arr)
+    return arr[len(arr) // 2]
+      
+
+a = [_ for _ in range(1, 11)]
+a.sort()
+a = shift(a)
+print(a)
+print(search(a))
