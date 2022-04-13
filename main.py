6def3839043d270e13b94d46d0f81b8e1837b109
Денис <103391833+VesDenis@users.noreply.github.com>
2022-04-13 02:28:05 +0000

    prime

--- main.py
+++ main.py
@@ -1,1 +1,7 @@
-Line 2
+from time import perf_counter
+
+start = perf_counter()
+primes = [i for i in range(5, 3000) if i % 6 == 5 or i % 6 == 1 and i % 5 != 0 and i % 3 != 0 and i % 7 != 0]
+end = perf_counter()
+print((end - start) * 1000)
+print(primes)
