2ebf9bfd0d6a56be06572ec9048d15b4fe470f4e
Денис <103391833+VesDenis@users.noreply.github.com>
2022-04-15 00:16:11 +0000

    search a letter

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-__str__
+class WordError(Exception):
+  def __init__(self):
+    self.message = 'This is your Exception message!'
+    super().__init__(self.message)
+
+def check_w_letter(word, letter):
+  if letter in word:
+    raise WordError
+  else:
+    print(word)
+
+letter = input()
+word = input()
+check_w_letter(word, letter)
