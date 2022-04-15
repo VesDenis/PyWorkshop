bd9a8300c41dd0637b1b35d75fe19ed3d7b0f7dd
Денис <103391833+VesDenis@users.noreply.github.com>
2022-04-15 00:07:08 +0000

    methods in exceptions

--- main.py
+++ main.py
@@ -1,12 +1,1 @@
-class NegativeSumException(Exception):
-  def __init__(self, x):
-    self.message = "Sum cannot be negative"
-    super().__init__(self.message)
-
-def sum_with_exceptions(a, b):
-  if a + b < 0:
-    raise NegativeSumException(a + b)
-  else:
-    print(a + b)
-
-sum_with_exceptions(2, -5)
+__str__
