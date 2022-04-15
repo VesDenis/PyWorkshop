af8ccc5dd456fd344d66766e9df796f3c83773b2
Денис <103391833+VesDenis@users.noreply.github.com>
2022-04-15 00:59:24 +0000

    points

--- main.py
+++ main.py
@@ -1,22 +1,9 @@
-class NotInBoundsError(Exception):
-  def __str__(self):
-    return 'There is an error!'
-
-def error_handling(func):
-  def wrapper(*args):
-    try:
-      n = func(*args)
-    except NotInBoundsError as err:
-      print(err)
-    else:
-      return n
-  return wrapper
-
-@error_handling
-def check_integer(num):
-  if 45 < num < 67:
-    raise NotInBoundsError
-  else:
-    print(num)
-
-check_integer(44)
+a = list(map(int, input().split()))
+ans = 0
+for i in range(len(a) - 1):
+  j = i + 1
+  while a[j] - a[i] <= 3 and j < len(a) - 1:
+    j += 1
+  if j != len(a) - 1:
+    ans += len(a[j:])
+print(ans)
