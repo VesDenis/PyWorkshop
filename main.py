2459f21925e869a8bd0ceafecf72475c84c44f2c
Денис <103391833+VesDenis@users.noreply.github.com>
2022-04-18 01:06:09 +0000

    doggy doggy

--- main.py
+++ main.py
@@ -1,15 +1,3 @@
-def get_prime_divs(n):
-  res = []
-  for i in range(2, int(n ** 0.5) + 1):
-    while not n % i:
-      if not i in res:
-        res.append(i)
-      n //= i
-    if n < i:
-      break
-  if n > 1:
-     res.append(n)
-  return res
-
-n = 999_999_999
-print(get_prime_divs(n))
+3
+0
+0
