eba69758de7645fedb7a5c6fed6ccde79598a01e
Денис <103391833+VesDenis@users.noreply.github.com>
2022-04-19 23:12:41 +0000

    most_rare

--- main.py
+++ main.py
@@ -1,4 +1,21 @@
-with open('countries.txt', 'a') as file:
-    file.write('\n')
-    file.write(input())
-    file.write('\n...')
+from collections import defaultdict, Counter
+
+s = 0
+count = defaultdict(int)
+with open('passwords.txt', 'r') as file:
+  for line in file.readlines():
+    for letter in line.strip():
+      count[letter] += 1
+      s += 1
+  count = Counter(count)
+  mi = s * count.most_common(1)[0][1]
+  ans = ''
+with open('passwords.txt', 'r') as file:
+  for line in file.readlines():
+    su = 0
+    for l in line.strip():
+      su += count[l]
+    if su < mi:
+      mi = su
+      ans = line
+print(ans)
