024da178ef06d3de8c936d38952dea47f09cb6d5
Денис <103391833+VesDenis@users.noreply.github.com>
2022-04-19 02:47:22 +0000

    solar

--- main.py
+++ main.py
@@ -1,1 +1,6 @@
-cyanmagentayellowkey color
+planets = ['Mercury', 'Venus', 'Earth', 'Mars', 'Jupiter', 'Saturn', 'Uranus', 'Neptune']
+
+with open('solar.txt', 'a') as file:
+  for planet in planets:
+    file.write(planet)
+    file.write('\n')
