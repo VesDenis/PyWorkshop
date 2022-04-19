837b73263ebaa0cbc36f9d6262fb1003a941c965
Денис <103391833+VesDenis@users.noreply.github.com>
2022-04-19 01:18:18 +0000

    how many

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 s = 0
-with open('summer.txt', 'r', encoding='utf-8') as file:
+with open('passwords.txt', 'r') as file:
   for word in file:
-    if word.strip() == 'summer':
       s += 1
 print(s)
