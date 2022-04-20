c724573133e99d90900610689a469b8d9eb20760
Денис <103391833+VesDenis@users.noreply.github.com>
2022-04-20 00:08:37 +0000

    response

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
-1023 to 65535
+import socket
+
+yet_another_socket = socket.socket()
+yet_another_socket.connect(('localhost', 1025))
+data = b'How deep does the rabbit socket go?'
+yet_another_socket.send(data)
+buffer_size = 1024
+response = yet_another_socket.recv(buffer_size)
