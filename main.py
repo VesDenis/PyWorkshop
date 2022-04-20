e91cef16ca26c620603191c93b43d8adf3cfcbeb
Денис <103391833+VesDenis@users.noreply.github.com>
2022-04-20 00:06:35 +0000

    connect 'b

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-def submit_data(data, client, address):
-  client.connect(address)
-  client.senr(data.encode())
+import socket
+
+your_sock = socket.socket()
+your_sock.connect(('deepthought.com', 4242))
+question = b'The answer to the meaning of life'
+your_sock.send(question)
