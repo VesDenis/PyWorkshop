a20779495c20a4bd81bfe1bed373af990e4c21ad
Денис <103391833+VesDenis@users.noreply.github.com>
2022-04-20 02:01:55 +0000

    letters

--- main.py
+++ main.py
@@ -1,8 +1,3 @@
-import socket
-
-yet_another_socket = socket.socket()
-yet_another_socket.connect(('localhost', 1025))
-data = b'How deep does the rabbit socket go?'
-yet_another_socket.send(data)
-buffer_size = 1024
-response = yet_another_socket.recv(buffer_size)
+w = [l for l in input()]
+for l in w:
+  print(l)
