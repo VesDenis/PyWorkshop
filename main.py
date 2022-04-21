77f8986d33f710954c40b525f6e2e51d114e3619
Денис <103391833+VesDenis@users.noreply.github.com>
2022-04-21 01:13:12 +0000

    trees

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
-a = [i * 2 for i in range(int(input()))]
-for n in a:
-  print(n)
+N ,D = map(int, input().split())
+d = list(map(int, input().split()))
+for i in range(len(d) - 1):
+  for j in range(i + 1, len(d)):
+    if sum(d[i:j]) >= D:
+      break
+  if sum(d[i:j]) == D:
+    print(i + 1, j + 1)
+    break
